ebdb10d96861da86aca3d804b53602b1afd59335
rudes <>
2023-10-15 10:39:49 +0000

    linting

--- commands/config.py
+++ commands/config.py
@@ -1,10 +1,16 @@
+"""
+Commands for managing the guild level config for osrs events
+"""
 import logging
+import json
+import discord
 import redis
-from dataclasses import dataclass
-from checks import is_mod
-from discord import SlashCommandGroup, Option, ApplicationContext
+
+from discord import SlashCommandGroup, ApplicationContext
 from discord.ext import commands
-from discord.commands import slash_command
+from discord.commands import Option
+
+from .checks import is_mod
 
 log = logging.getLogger(__name__)
 
@@ -15,37 +21,49 @@ log = logging.getLogger(__name__)
 }
 """
 
+
 class Config(commands.Cog):
+    """
+    Config class housing commands for guild level config
+    """
+
     def __init__(self, bot):
         self.bot = bot
-        self.db = redis.StrictRedis(host='db')
+        self.db = redis.StrictRedis(host="db")
 
     config = SlashCommandGroup(
-        'config',
-        'Change the settings for the entire server. CAREFUL.',
-        checks=[is_mod()]
+        "config",
+        "Change the settings for the entire server. CAREFUL.",
+        checks=[is_mod()],
     )
 
+    def save_config(self, guild_id, guild_config):
+        """save the config to the db"""
+        self.db.set(str(guild_id), guild_config)
+
     async def setup_server(self, ctx: ApplicationContext):
+        """prepare the discord for events"""
         # ask for approval
         # create mod role
         # create event category and event channels
         guild_config = {
-            'mod_role_id': 0,
-            'event_channel_id': 0,
+            "mod_role_id": 0,
+            "event_channel_id": 0,
         }
-        self.db.set(str(ctx.guild.id), guild_config)
+        self.save_config(str(ctx.guild.id), guild_config)
 
     async def get_config(self, ctx: ApplicationContext):
+        """get the config from the db"""
         raw = self.db.get(str(ctx.guild.id))
         if not raw:
             return await self.setup_server(ctx)
-        return json.loads(raw.decode('utf-8'))
+        return json.loads(raw.decode("utf-8"))
 
     @config.command()
-    async def set_mod_role(self,
+    async def set_mod_role(
+        self,
         ctx: ApplicationContext,
-        id: Option(int, 'role id for the events moderators')
+        role: Option(discord.Role, "role for the events moderators"),
     ):
         """
         Set the mod_role_id for this guild.
@@ -55,22 +73,20 @@ class Config(commands.Cog):
             guild_config = await self.get_config(ctx)
             if not guild_config:
                 return
-            guild_config['mod_role_id'] = id
+            guild_config["mod_role_id"] = role.id
             self.save_config(ctx.guild.id, guild_config)
-            await ctx.reply(f"mod_role updated to {id}.")
+            await ctx.reply(f"mod_role updated to {role.id}.")
         except Exception as e:
             log.exception(f"mod_role,{type(e)} error occured,{e}")
 
     @config.command()
     async def get_mod_role(self, ctx: ApplicationContext):
-        """
-        Get the mod_role_id for this guild.
-        """
+        """Get the mod_role_id for this guild"""
         try:
             guild_config = await self.get_config(ctx)
             if not guild_config:
                 return
-            id = guild_config['mod_role_id']
-            await ctx.reply(f"mod_role id is {id}.")
+            mod_role_id = guild_config["mod_role_id"]
+            await ctx.reply(f"mod_role id is {mod_role_id}.")
         except Exception as e:
             log.exception(f"mod_role,{type(e)} error occured,{e}")
--- handlers/ready.py
+++ handlers/ready.py
@@ -1,3 +1,4 @@
+""" simple listener for on_ready event """
 import logging
 import discord
 
@@ -5,14 +6,18 @@ from discord.ext import commands
 
 log = logging.getLogger(__name__)
 
+
 class Ready(commands.Cog):
+    """simple listener for on_ready event"""
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
+        """simple listener for on_ready event"""
         try:
             await self.bot.change_presence(activity=discord.Game(name="/help"))
-            log.info('on_ready,presence state set')
+            log.info("on_ready,presence state set")
         except Exception as e:
-            log.error('on_ready,{0} error occured,{1}'.format(type(e), e))
+            log.exception(f"on_ready,{type(e)} error occured,{e}")
