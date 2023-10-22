239c8cbfdee26db8e306a6db01eaabf4c66687df
rudes <>
2023-10-22 10:42:28 +0000

    fix mod_role and add event_channel commands

--- commands/config.py
+++ commands/config.py
@@ -39,7 +39,8 @@ class Config(commands.Cog):
 
     def save_config(self, guild_id, guild_config):
         """save the config to the db"""
-        self.db.set(str(guild_id), guild_config)
+        config_json = json.dumps(guild_config)
+        self.db.set(str(guild_id), config_json)
 
     async def setup_server(self, ctx: ApplicationContext):
         """prepare the discord for events"""
@@ -47,8 +48,8 @@ class Config(commands.Cog):
         # create mod role
         # create event category and event channels
         guild_config = {
-            "mod_role_id": 0,
-            "event_channel_id": 0,
+            "mod_role_id": 1159942632605237298,
+            "event_channel_id": 1159949415335854160,
         }
         self.save_config(str(ctx.guild.id), guild_config)
 
@@ -59,15 +60,20 @@ class Config(commands.Cog):
             return await self.setup_server(ctx)
         return json.loads(raw.decode("utf-8"))
 
-    @config.command()
+    mod_role = config.create_subgroup(
+        "mod_role",
+        "View/Change the Moderator Role for this Discord",
+        checks=[is_mod()],
+    )
+
+    @mod_role.command(name='set')
     async def set_mod_role(
         self,
         ctx: ApplicationContext,
         role: Option(discord.Role, "role for the events moderators"),
     ):
         """
-        Set the mod_role_id for this guild.
-        People with this role will have control of events.
+        Set the Moderator Role for this Discord. People with this role will have control of events.
         """
         try:
             guild_config = await self.get_config(ctx)
@@ -75,18 +81,57 @@ class Config(commands.Cog):
                 return
             guild_config["mod_role_id"] = role.id
             self.save_config(ctx.guild.id, guild_config)
-            await ctx.reply(f"mod_role updated to {role.id}.")
+            await ctx.respond(f"Moderator Role updated to @**{role.name}**.")
         except Exception as e:
-            log.exception(f"mod_role,{type(e)} error occured,{e}")
+            log.exception(f"set_mod_role,{type(e)} error occured,{e}")
 
-    @config.command()
-    async def get_mod_role(self, ctx: ApplicationContext):
-        """Get the mod_role_id for this guild"""
+    @mod_role.command(name='view')
+    async def view_mod_role(self, ctx: ApplicationContext):
+        """Get the Moderator Role for this Discord"""
         try:
             guild_config = await self.get_config(ctx)
             if not guild_config:
                 return
             mod_role_id = guild_config["mod_role_id"]
-            await ctx.reply(f"mod_role id is {mod_role_id}.")
+            mod_role = ctx.guild.get_role(int(mod_role_id))
+            await ctx.respond(f"Moderator Role is @**{mod_role.name}**.")
         except Exception as e:
-            log.exception(f"mod_role,{type(e)} error occured,{e}")
+            log.exception(f"view_mod_role,{type(e)} error occured,{e}")
+
+    event_channel = config.create_subgroup(
+        "event_channel",
+        "View/Change the Event Forum Channel for this Discord",
+        checks=[is_mod()],
+    )
+
+    @event_channel.command(name='set')
+    async def set_event_channel(
+        self,
+        ctx: ApplicationContext,
+        chan: Option(discord.ForumChannel, "forum channel for events"),
+    ):
+        """
+        Set the Event Forum Channel for this Discord.
+        """
+        try:
+            guild_config = await self.get_config(ctx)
+            if not guild_config:
+                return
+            guild_config["event_channel_id"] = chan.id
+            self.save_config(ctx.guild.id, guild_config)
+            await ctx.respond(f"Event Forum Channel updated to {chan.mention}.")
+        except Exception as e:
+            log.exception(f"set_event_channel,{type(e)} error occured,{e}")
+
+    @event_channel.command(name='view')
+    async def view_event_channel(self, ctx: ApplicationContext):
+        """Get the Moderator Role for this Discord"""
+        try:
+            guild_config = await self.get_config(ctx)
+            if not guild_config:
+                return
+            event_chan_id = guild_config["event_channel_id"]
+            chan = ctx.guild.get_channel(int(event_chan_id))
+            await ctx.respond(f"Event Forum Channel is {chan.mention}.")
+        except Exception as e:
+            log.exception(f"view_event_channel,{type(e)} error occured,{e}")
